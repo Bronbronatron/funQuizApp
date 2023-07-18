6a4999b12a53fbc36cfe4cd3c6ecac04607c743c
Bronbronatron <bronwyn93@gmail.com>
2023-07-18 10:15:48 +0200

    Initial radio button added

--- src/main/java/com/bronwyn/movieRecommendation/MainController.java
+++ src/main/java/com/bronwyn/movieRecommendation/MainController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.bronwyn.movieRecommendation.question.Question;
 import com.bronwyn.movieRecommendation.question.QuestionService;
@@ -23,6 +25,17 @@ public class MainController {
 	String getPage(Model model) {
 		 List<Question> questions = questionService.getQuestion();
 	        model.addAttribute("questions", questions);
+	        model.addAttribute("question", new Question());
 		return "page";
 	}
-}
+	
+	   @PostMapping(path = "api/v1/page")
+	    String processForm(@ModelAttribute List<Question> questions) {
+	        for (Question question : questions) {
+	            String selectedChoice = question.getSelectedChoice();
+	        }
+	        return "redirect:/result"; // Change "/result" to the appropriate URL for the result page
+	    }
+	}
+
+
